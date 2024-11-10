f6440cdee52c34ee28fcc6e464607eb983122fca
nutterthanos <minecraftitsover90@gmail.com>
2024-11-11 06:01:50 +1030

    Update grab_mayhemids.py

--- grab_mayhemids.py
+++ grab_mayhemids.py
@@ -8,7 +8,7 @@ from xml.etree import ElementTree as ET
 
 # Define the range for applicationUserId
 START_ID = 370000000
-END_ID = 375000000
+END_ID = 374000000
 
 # Maximum concurrent requests and retries
 MAX_CONCURRENT_REQUESTS = 25000
